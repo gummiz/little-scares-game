ed0600a7dae3006c35d08aaf60c1c31dc15760b4
Stefan.Kummerlöw <gummiz@gmx.net>
2024-01-29 11:15:16 +0100

    player movement with boarder limitation added

--- Readme.md
+++ Readme.md
@@ -1,24 +1,23 @@
-# Milestones
-
 1. Basics
     - [x]  Board with BG
-    - [ ]  Bounding box ([getBoundingClientRect()](https://www.youtube.com/watch?v=MKpZadkuT-0), [Browser support](https://caniuse.com/?search=getBoundingClientRect))
+    - [x]  Bounding box ([getBoundingClientRect()](https://www.youtube.com/watch?v=MKpZadkuT-0), [Browser support](https://caniuse.com/?search=getBoundingClientRect))
 2. Character Ghost
-    - [ ]  add character
-    - [ ]  place character
-    - [ ]  create movement X/Y ([Diagonal](https://stackoverflow.com/questions/14178782/how-can-i-make-my-character-move-diagonally))
-    - [ ]  respect bounding box
-    - [ ]  Eventlistener
-3. NPC
-    - [ ]  add NPC
-    - [ ]  place NPC
-    - [ ]  Eventlistener
-4. Environment
+    - [x]  add character
+    - [x]  place character
+    - [x]  create movement X/Y
+    - [ ]  Extra: Diagonal Movement ([Diagonal](https://stackoverflow.com/questions/14178782/how-can-i-make-my-character-move-diagonally))
+    - [x]  respect bounding box
+    - [x]  Eventlistener
+3. Environment
     - [ ]  interaction with elements on the board
     - [ ]  if ghost triggers scare, person turns to POI
     - [ ]  person walks to POI
     - [ ]  Ghost visible / invisble when interacting
     - [ ]  Triggering of NPC
+4. NPC
+    - [ ]  add NPC
+    - [ ]  place NPC
+    - [ ]  Eventlistener
 5. Collision system & 
     - [ ]  if ghost is in POI and Visible = loose game
 6. UI
--- js/main.js
+++ js/main.js
@@ -1,50 +1,86 @@
 class Ghost {
   constructor() {
-    this.x = 50;
-    this.y = 50;
-    this.heigth = 1;
-    this.width = 1;
-    this.characterStartup()
+    this.x = 300;
+    this.y = 300;
+    this.heigth = 30;
+    this.width = 30;
+    this.characterStartup();
   }
 
   characterStartup() {
-    const ghost = document.createElement("div")
-    ghost.setAttribute("id", "ghost")
-    
-    // Ghost Size
-    
+    this.ghost = document.createElement("div");
+    this.ghost.setAttribute("id", "ghost");
 
+    // Ghost Size
+    this.ghost.style.width = this.width + "px";
+    this.ghost.style.height = this.heigth + "px";
+
+    // place Ghost
+    this.board = document.getElementById("board");
+    this.board.appendChild(this.ghost);
+
+    // set position of Ghost
+    this.ghost.style.position = "absolute";
+    this.ghost.style.top = this.x - this.heigth / 2 + "px";
+    this.ghost.style.left = this.y - this.width / 2 + "px";
   }
 
   moveUp() {
+    if (this.ghost.getBoundingClientRect().top > 40) {
+      this.y -= 10;
+      this.ghost.style.top = this.y - this.width / 2 + "px";
+    }
   }
   moveDown() {
-    /*...*/
+    if (this.ghost.getBoundingClientRect().bottom < 600) {
+      this.y += 10;
+      this.ghost.style.top = this.y - this.width / 2 + "px";
+    }
+  }
+  moveLeft() {
+    if (this.ghost.getBoundingClientRect().left > 35) {
+      this.x -= 10;
+      this.ghost.style.left = this.x - this.width / 2 + "px";
+    }
+  }
+  moveRight() {
+    if (this.ghost.getBoundingClientRect().right < 600) {
+      this.x += 10;
+      this.ghost.style.left = this.x - this.width / 2 + "px";
+    }
   }
 }
 
 class Environment {
-  constructor(/*...*/) {}
+  constructor() {}
 }
 
 class NPC {
-  constructor(/*...*/) {}
+  constructor() {}
 }
-
 
 const player = new Ghost();
 // const environment = new Environment());
 // const npc = new NPC();
 
-
-//Controll 
-window.addEventListener('keydown', function(event) {
-    switch (event.key) {
-        case 'ArrowUp':
-            player.moveUp();
-            break;
-        case 'ArrowDown':
-            player.moveDown();
-            break;
-    }
+//Controls
+window.addEventListener("keydown", function (event) {
+    console.log("Ghost Position:", this.ghost.getBoundingClientRect());
+  switch (event.key) {
+    case "ArrowUp":
+      player.moveUp();
+      break;
+    case "ArrowDown":
+      player.moveDown();
+      break;
+    case "ArrowLeft":
+      player.moveLeft();
+      break;
+    case "ArrowRight":
+      player.moveRight();
+      break;
+    case "Spacebar":
+      player.interact();
+      break;
+  }
 });
